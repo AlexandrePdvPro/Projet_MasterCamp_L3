88a9af623fccd6b927b53b23128a6de01d3b7d38
Vincent Petit <vincent.petit@efrei.net>
2021-06-25 15:57:32 +0200

    Update home & vote page

---
+++ client/src/components/CVote.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="hero is-primary is-fullheight">
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns is-centered is-multiline">
+					<div class="column">Vote</div>
+					<div class="column"></div>
+					<div class="column"></div>
+				</div>
+			</div>
+		</div>
+		<div class="hero-foot">Hello</div>
+	</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "CVote",
+});
+</script>
+
+<style scoped></style>
