9cf8e78ff12b2fdce2dc7a3d697eab93c3cc314a
Alexandre <alex.piedvache@laposte.net>
2021-07-06 17:23:53 +0200

    Confirm mdp

--- client/src/components/RForm.vue
+++ client/src/components/RForm.vue
@@ -32,18 +32,30 @@
 								</div>
 							</div>
 
-							<div class="field">
-								<label class="label">Email</label>
-								<div class="control has-icons-left has-icons-right">
-									<input
-										v-model="customerData.email"
-										class="input"
-										type="text"
-										placeholder="Email"
-										required
-									/>
-								</div>
-							</div>
+              <div class="field">
+                <label class="label">Email</label>
+                <div class="control has-icons-left has-icons-right">
+                  <input
+                      class="input"
+                      type="email"
+                      placeholder="Adresse email"
+                      @input="emailField.handleChange"
+                      @blur="emailField.handleBlur"
+                      :value="emailField.value"
+                  />
+                </div>
+                <p
+                    class="has-text-danger"
+                    :style="{
+                      visibility:
+                        emailField.meta.touched && !emailField.meta.valid
+                          ? 'visible'
+                          : 'hidden',
+                    }"
+                >
+                  {{ emailField.errorMessage || 'Ce champ est requis' }}
+                </p>
+              </div>
 
 							<div class="field">
 								<label class="label">Numéro d'identité</label>
@@ -58,27 +70,65 @@
 								</div>
 							</div>
 
-							<div class="field">
-								<label class="label">Mot de passe</label>
-								<div class="control has-icons-left has-icons-right">
-									<input
-										v-model="customerData.password"
-										class="input"
-										type="text"
-										placeholder="Mot de passe"
-										required
-									/>
-								</div>
-							</div>
+              <div class="field">
+                <label class="label">Mot de passe</label>
+                <div class="control has-icons-left has-icons-right">
+                  <input
+                      class="input"
+                      type="password"
+                      placeholder="Password"
+                      @input="passwordField.handleChange($event)"
+                      @blur="passwordField.handleBlur"
+                      :value="passwordField.value"
+                  />
+                </div>
+                <p
+                    class="has-text-danger"
+                    :style="{
+                      visibility:
+                        passwordField.meta.touched && !passwordField.meta.valid
+                          ? 'visible'
+                          : 'hidden',
+                    }"
+                >
+                  {{ passwordField.errorMessage || 'Ce champ est requis' }}
+                </p>
+              </div>
 
-							<div class="field">
-								<div class="control">
-									<label class="checkbox">
-										<input type="checkbox" required />
-										J'accepte les <a href="#">conditions d'utilisation</a>
-									</label>
-								</div>
-							</div>
+              <div class="field">
+                <label class="label">Confirmer le mot de passe</label>
+                <div class="control has-icons-left has-icons-right">
+                  <input
+                      class="input"
+                      type="password"
+                      placeholder="Confirm Password"
+                      @input="confirmPasswordField.handleChange"
+                      @blur="confirmPasswordField.handleBlur"
+                      :value="confirmPasswordField.value"
+                  />
+                </div>
+                <p
+                    class="has-text-danger"
+                    :style="{
+                      visibility:
+                        confirmPasswordField.meta.touched &&
+                        !confirmPasswordField.meta.valid
+                          ? 'visible'
+                          : 'hidden',
+                    }"
+                >
+                  {{ confirmPasswordField.errorMessage || 'Ce champ est requis' }}
+                </p>
+              </div>
+
+              <div class="field">
+                <div class="control">
+                  <label class="checkbox">
+                    <input type="checkbox" required />
+                    J'accepte les <a href="#">conditions d'utilisation</a>
+                  </label>
+                </div>
+              </div>
 
 							<div class="field is-grouped is-justify-content-center">
 								<div class="control">
@@ -97,10 +147,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, computed} from "vue";
 import axios from "axios";
 import { server } from "../helper";
 import router from "../router";
+import {useField, useForm} from "vee-validate";
 
 interface newUser {
   nom: string;
@@ -121,6 +172,28 @@ export default defineComponent({
       password: "",
     });
 
+    const { meta: formMeta, handleSubmit } = useForm();
+    const emailField = reactive(useField('email', 'email'));
+    const passwordField = reactive(useField('password', 'password'));
+    const customerData: newUser = reactive({nom:'', prenom:'', email:'', numero_id: '', password:'' })
+
+    const confirmPasswordValidator = computed(() => {
+      return 'confirmPassword:password';
+    });
+
+    const confirmPasswordField = reactive(useField('confirmPassword', confirmPasswordValidator));
+
+    const submitForm = handleSubmit((formValues:any) => {
+      customerData.email = formValues.email;
+      customerData.password = formValues.password;
+      console.log('customerData: ', customerData);
+      submitToServer(customerData);
+      router.push({ name: "Home" });
+      customerData.email = '';
+      customerData.password = ''
+      console.log('customerData: ', customerData);
+    });
+
     const Register = function () {
       console.log(customerData);
       submitToServer(customerData);
@@ -128,17 +201,20 @@ export default defineComponent({
     };
 
     const submitToServer = function (data: newUser) {
-      axios
-        .put(`${server.baseURL}/api/users/add/user`, data)
-        .then((response) => console.log(customerData));
-    };
+          axios.put(`${server.baseURL}/api/add/user`, data)
+    }
 
     return {
       Register,
       submitToServer,
       customerData,
-    };
-  },
+      emailField,
+      passwordField,
+      confirmPasswordField,
+      submitForm,
+      formMeta,
+    }
+  }
 });
 </script>
 
