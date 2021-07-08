bd8fc9802d966d4e732311230150386063ceed92
Alexandre Pdv <alexandre.piedvache@efrei.net>
2021-07-08 18:11:09 +0200

    Avancement créations de sessions

--- client/src/components/Clogin.vue
+++ client/src/components/Clogin.vue
@@ -1,80 +1,88 @@
 <template>
-	<section class="hero is-fullheight is-primary">
-		<div class="hero-body">
-			<div class="container has-text-centered">
-				<div class="column is-4 is-offset-4">
-					<h3 class="title">Se connecter</h3>
-					<div class="box">
-						<form @submit.prevent="submitForm">
-							<div class="field">
-								<label class="label">Email</label>
-								<div class="control has-icons-left has-icons-right">
-									<input
-										class="input"
-										type="email"
-										placeholder="Adresse email"
-										@input="emailField.handleChange"
-										@blur="emailField.handleBlur"
-										:value="emailField.value"
-									/>
-								</div>
-								<p
-									class="has-text-danger"
-									:style="{
-										visibility:
-											emailField.meta.touched && !emailField.meta.valid ? 'visible' : 'hidden',
-									}"
-								>
-									{{ emailField.errorMessage || "Ce champ est requis" }}
-								</p>
-							</div>
+  <section class="hero is-fullheight is-primary">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-4 is-offset-4">
+          <h3 class="title">Se connecter</h3>
+          <div class="box">
+            <form @submit.prevent="submitForm">
+              <div class="field">
+                <label class="label">Email</label>
+                <div class="control has-icons-left has-icons-right">
+                  <input
+                    class="input"
+                    type="email"
+                    placeholder="Adresse email"
+                    @input="emailField.handleChange"
+                    @blur="emailField.handleBlur"
+                    :value="emailField.value"
+                  />
+                </div>
+                <p
+                  class="has-text-danger"
+                  :style="{
+                    visibility:
+                      emailField.meta.touched && !emailField.meta.valid
+                        ? 'visible'
+                        : 'hidden',
+                  }"
+                >
+                  {{ emailField.errorMessage || "Ce champ est requis" }}
+                </p>
+              </div>
 
-							<div class="field">
-								<label class="label">Mot de passe</label>
-								<div class="control has-icons-left has-icons-right">
-									<input
-										class="input"
-										type="password"
-										placeholder="Password"
-										@input="passwordField.handleChange($event)"
-										@blur="passwordField.handleBlur"
-										:value="passwordField.value"
-									/>
-								</div>
-								<p
-									class="has-text-danger"
-									:style="{
-										visibility:
-											passwordField.meta.touched && !passwordField.meta.valid
-												? 'visible'
-												: 'hidden',
-									}"
-								>
-									{{ passwordField.errorMessage || "Ce champ est requis" }}
-								</p>
-							</div>
+              <div class="field">
+                <label class="label">Mot de passe</label>
+                <div class="control has-icons-left has-icons-right">
+                  <input
+                    class="input"
+                    type="password"
+                    placeholder="Password"
+                    @input="passwordField.handleChange($event)"
+                    @blur="passwordField.handleBlur"
+                    :value="passwordField.value"
+                  />
+                </div>
+                <p
+                  class="has-text-danger"
+                  :style="{
+                    visibility:
+                      passwordField.meta.touched && !passwordField.meta.valid
+                        ? 'visible'
+                        : 'hidden',
+                  }"
+                >
+                  {{ passwordField.errorMessage || "Ce champ est requis" }}
+                </p>
+              </div>
 
-							<div class="field is-grouped is-justify-content-center">
-								<div class="control">
-									<button class="button is-link" :disabled="!formMeta.valid" type="submit">
-										Se connecter
-									</button>
-								</div>
-								<div class="control">
-									<button class="button is-link is-light">Annuler</button>
-								</div>
-							</div>
-						</form>
-					</div>
-					<router-link to="/reset-password">Mot de passe oublié ?</router-link>
-				</div>
-			</div>
-		</div>
-	</section>
+              <div class="field is-grouped is-justify-content-center">
+                <div class="control">
+                  <button
+                    class="button is-link"
+                    :disabled="!formMeta.valid"
+                    type="submit"
+                  >
+                    Se connecter
+                  </button>
+                </div>
+                <div class="control">
+                  <router-link class="button is-link is-light" to="/"
+                    >Annuler</router-link
+                  >
+                </div>
+              </div>
+            </form>
+          </div>
+          <router-link to="/reset-password">Mot de passe oublié ?</router-link>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive } from "vue";
+import { defineComponent, reactive } from "vue";
 import { useStore } from "vuex";
 import { useField, useForm } from "vee-validate";
 import axios from "axios";
@@ -82,50 +90,50 @@ import { server } from "../helper";
 import router from "../router";
 
 interface user {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 export default defineComponent({
-	name: "Clogin",
-	setup() {
-		const store = useStore();
+  name: "Clogin",
+  setup() {
+    const store = useStore();
 
-		const { meta: formMeta, handleSubmit } = useForm();
-		const emailField = reactive(useField("email", "email"));
-		const passwordField = reactive(useField("password", "password"));
-		const customerData: user = reactive({ email: "", password: "" });
+    const { meta: formMeta, handleSubmit } = useForm();
+    const emailField = reactive(useField("email", "email"));
+    const passwordField = reactive(useField("password", "password"));
+    const customerData: user = reactive({ email: "", password: "" });
 
-		const submitForm = handleSubmit((formValues: any) => {
-			customerData.email = formValues.email;
-			customerData.password = formValues.password;
-			console.log("customerData: ", customerData);
-			submitToServer(customerData);
-			store.commit("setIsConnected", true);
-			router.push({ name: "Home" });
-			customerData.email = "";
-			customerData.password = "";
-			console.log("customerData: ", customerData);
-		});
+    const submitForm = handleSubmit((formValues: any) => {
+      customerData.email = formValues.email;
+      customerData.password = formValues.password;
+      console.log("customerData: ", customerData);
+      submitToServer(customerData);
+      store.commit("setIsConnected", true);
+      router.push({ name: "Home" });
+      customerData.email = "";
+      customerData.password = "";
+      console.log("customerData: ", customerData);
+    });
 
-		const submitToServer = function (data: user) {
-			axios.post(`${server.baseURL}/api/auth/login`, data);
-		};
+    const submitToServer = function (data: user) {
+      axios.post(`${server.baseURL}/api/auth/login`, data);
+    };
 
-		return {
-			submitToServer,
-			customerData,
-			emailField,
-			passwordField,
-			submitForm,
-			formMeta,
-		};
-	},
+    return {
+      submitToServer,
+      customerData,
+      emailField,
+      passwordField,
+      submitForm,
+      formMeta,
+    };
+  },
 });
 </script>
 
 <style scoped>
 ::placeholder {
-	color: black;
+  color: black;
 }
 </style>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,53 +1,62 @@
 <template>
-	<nav class="navbar is-dark is-fixed-top" role="navigation" aria-label="main navigation">
-		<div class="navbar-brand">
-			<router-link class="navbar-item" to="/">
-				<img src="../assets/LogoTk.svg" width="112" height="28" alt="" />
-			</router-link>
+  <nav
+    class="navbar is-dark is-fixed-top"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <img src="../assets/LogoTk.svg" width="112" height="28" alt="" />
+      </router-link>
 
-			<a
-				role="button"
-				class="navbar-burger"
-				aria-label="menu"
-				aria-expanded="false"
-				data-target="navbarBasicExample"
-			>
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-			</a>
-		</div>
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
 
-		<div id="navbarBasicExample" class="navbar-menu">
-			<div class="navbar-start">
-				<router-link class="navbar-item" to="/vote">Vote</router-link>
-			</div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/vote">Vote</router-link>
+      </div>
 
-			<div class="navbar-end" v-if="!isConnected">
-				<div class="navbar-item">
-					<div class="buttons">
-						<router-link class="button is-link" to="/Register">S'enregistrer</router-link>
-						<router-link class="button is-secondary" to="/Login">Se connecter</router-link>
-					</div>
-				</div>
-			</div>
-		</div>
+      <div class="navbar-end" v-if="!isConnected">
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link class="button is-link" to="/Register"
+              >S'enregistrer</router-link
+            >
+            <router-link class="button is-secondary" to="/Login"
+              >Se connecter</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
 
-		<div class="navbar-end" v-if="isConnected">
-			<div class="navbar-item">
-				<div class="buttons">
-					<router-link class="button is-rounded" to="/Panel">
-						<span class="icon">
-							<i class="fa fa-github"></i>
-						</span>
-					</router-link>
-					<router-link class="button is-link" to="/Login" @click="deconnexion"
-						>Se déconnecter</router-link
-					>
-				</div>
-			</div>
-		</div>
-	</nav>
+    <div class="navbar-end" v-if="isConnected">
+      <div class="navbar-item">
+        <div class="buttons">
+          <router-link v-if="isAdmin" class="button is-link" to="/Panel">
+            Créer Session
+          </router-link>
+          <router-link
+            class="button is-secondary"
+            to="/Login"
+            @click="deconnexion"
+            >Se déconnecter</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -55,47 +64,52 @@ import { useStore } from "vuex";
 import { computed } from "vue";
 
 document.addEventListener("DOMContentLoaded", () => {
-	// Get all "navbar-burger" elements
-	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
-	// Check if there are any navbar burgers
-	if ($navbarBurgers.length > 0) {
-		// Add a click event on each of them
-		$navbarBurgers.forEach((el) => {
-			el.addEventListener("click", () => {
-				// Get the target from the "data-target" attribute
-				const target = el.dataset.target;
-				const $target = document.getElementById(target);
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-				el.classList.toggle("is-active");
-				$target.classList.toggle("is-active");
-			});
-		});
-	}
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
 });
 export default {
-	name: "Navbar.vue",
-	setup() {
-		const store = useStore();
-		const isConnected = computed(() => store.state.isConnected);
+  name: "Navbar.vue",
+  setup() {
+    const store = useStore();
+    const isConnected = computed(() => store.state.isConnected);
+    const isAdmin = true;
 
-		function deconnexion() {
-			store.commit("setIsConnected", false);
-		}
+    function deconnexion() {
+      store.commit("setIsConnected", false);
+    }
 
-		return {
-			isConnected,
-			deconnexion,
-		};
-	},
+    return {
+      isConnected,
+      isAdmin,
+      deconnexion,
+    };
+  },
 };
 </script>
 
 <style scoped>
 .navbar-dropdown,
 .navbar-menu {
-	background-color: #363636;
-	border-top: 0px;
+  background-color: #363636;
+  border-top: 0px;
 }
 </style>
